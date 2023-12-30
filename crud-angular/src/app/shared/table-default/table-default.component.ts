46ac6fa603db43381b81b5c59959db3981d65bd1
martins86 <martins86@gmail.com>
2023-12-30 21:58:10 +0000

    ng g c shared/table-default

---
+++ crud-angular/src/app/shared/table-default/table-default.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-default',
+  templateUrl: './table-default.component.html',
+  styleUrls: ['./table-default.component.scss']
+})
+export class TableDefaultComponent { }
--- crud-angular/src/app/shared/table-default/table-default.module.ts
+++ crud-angular/src/app/shared/table-default/table-default.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TableDefaultComponent } from './table-default.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    TableDefaultComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TableDefaultComponent
   ]
 })
 export class TableDefaultModule { }
