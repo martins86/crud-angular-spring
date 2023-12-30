25274a41082e52c80244419ecf35fea5101383e4
martins86 <martins86@gmail.com>
2023-12-30 21:26:24 +0000

    ng g c pages/courses + config routing

--- crud-angular/src/app/pages/courses/courses.module.ts
+++ crud-angular/src/app/pages/courses/courses.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CoursesComponent
+  ],
   imports: [
     CommonModule,
     CoursesRoutingModule
+  ],
+  exports: [
+    CoursesComponent
   ]
 })
 export class CoursesModule { }
