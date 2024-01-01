f32a60c03610a50f8a374926e04f099ea80ab07c
martins86 <martins86@gmail.com>
2024-01-01 22:57:56 +0000

    add new package for run tests and new config

--- crud-angular/karma.conf.js
+++ crud-angular/karma.conf.js
@@ -1,44 +1,85 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+      require("karma-spec-reporter"),
     ],
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+        failSpecWithNoExpectations: true,
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false,
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true,
     },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/crud-angular'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+    thresholds: {
+      emitWarning: false,
+      global: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80,
+      },
+      each: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80,
+      },
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml", "spec"],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: ["--headless", "--no-sandbox", "--remote-debugging-port=9222"],
+      },
+      ChromeDebug: {
+        base: "Chrome",
+        flags: ["--remote-debugging-port=9222"],
+        debug: true,
+      },
+      ChromeHeadlessDocker: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"],
+      },
+    },
+    browserDisconnectTolerance: 8,
+    browserNoActivityTimeout: 60000,
+    browserDisconnectTimeout: 20000,
+    captureTimeout: 210000,
+    specReporter: {
+      maxLogLines: 5,
+      suppressErrorSummary: false,
+      suppressFailed: false,
+      suppressPassed: false,
+      suppressSkipped: false,
+      showSpecTiming: true,
+      failFast: false,
+      prefixes: {
+        success: "    OK: ",
+        failure: "FAILED: ",
+        skipped: "SKIPPED: ",
+      },
+    },
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage/crud-angular"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }, { type: "lcov" }],
+      fixWebpackSourcePaths: true,
+    },
+    logLevel: config.LOG_INFO,
   });
 };
