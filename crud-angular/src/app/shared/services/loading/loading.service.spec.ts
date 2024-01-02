7ff4bbb0542ee79caed8f4806f3f86fe0af0e692
martins86 <martins86@gmail.com>
2024-01-02 22:09:53 +0000

    add loading service

---
+++ crud-angular/src/app/shared/services/loading/loading.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { LoadingService } from './loading.service';
+
+describe('Testes do LoadingService', () => {
+    let service: LoadingService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(LoadingService);
+    });
+
+    it('Deve inicializar o LoadingService', () => {
+        expect(service).toBeTruthy();
+    });
+});
