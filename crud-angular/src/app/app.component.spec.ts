bc8493b7522554f7a06656bf337bbfe1703d6186
martins86 <martins86@gmail.com>
2024-01-02 22:13:09 +0000

    add tests and interceptor loading

--- crud-angular/src/app/app.component.spec.ts
+++ crud-angular/src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 const material = [MatToolbarModule, MatButtonModule, MatIconModule];
+
+import { LoadingModule } from './shared/components/loading/loading.module';
+
+const shared = [LoadingModule];
 
 import { CoursesModule } from './pages/courses/courses.module';
 
@@ -18,7 +23,9 @@ describe('Testes do AppComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
+        HttpClientTestingModule,
         ...material,
+        ...shared,
         ...pages
       ],
       declarations: [
--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { LoadingInterceptor } from './core/interceptors/loading-indicator/loading-indicator.interceptor';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 const material = [MatToolbarModule, MatButtonModule, MatIconModule];
+
+import { LoadingModule } from './shared/components/loading/loading.module';
+
+const shared = [LoadingModule];
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,9 +32,16 @@ const pages = [CoursesModule];
     BrowserAnimationsModule,
     HttpClientModule,
     ...material,
+    ...shared,
     ...pages
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
