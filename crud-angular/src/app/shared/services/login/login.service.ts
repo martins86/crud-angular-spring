3b90ac8e1c82b91f4a49ac0f64503de917c8be4d
martins86 <martins86@gmail.com>
2024-03-21 19:10:35 +0000

    feat: service login

---
+++ crud-angular/src/app/shared/services/login/login.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { tap } from 'rxjs';
+
+import { LoginResponse } from '../../types/input-types.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  constructor(private httpClient: HttpClient) { }
+
+  login(name: string, password: string) {
+    return this.httpClient.post<LoginResponse>("/login", { name, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
+  }
+}
