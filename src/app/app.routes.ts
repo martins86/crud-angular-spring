ef475ef76ded566025075a0efd69a9938313a147
martins86 <martins86@gmail.com>
2024-04-21 15:48:34 +0000

    chore: limpando e documentando itens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,41 @@ import { Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('@pages/auth/auth.routes').then((m) => m.AUTH_ROUTES),
-  },
-];
+export const routes: Routes = [];
 
+/**
+ * Customizando title da pagina por URL.
+ *
+ * @usageNotes
+ *
+ * ### Como Usar
+ *
+ * ```routes
+ * {
+ *  path: 'login',
+ *  title: 'Pagina de Login',
+ *  component: LoginComponent,
+ * },
+ * {
+ *  path: 'home',
+ *  title: 'Pagina Inicial',
+ *  loadChildren: () => import('@pages/home/home.routes').then((m) => m.home),
+ * },
+ * {
+ *  path: '',
+ *  redirectTo: '/login',
+ *  pathMatch: 'full',
+ * },
+ * ```
+ *
+ * ### Resultado URL /login
+ *
+ * ```html
+ * <title>Poc Angular - Pagina de Login</title>
+ * ```
+ *
+ * @publicApi
+ */
 @Injectable()
 export class TemplatePageTitleStrategy extends TitleStrategy {
   constructor(private readonly title: Title) {
--- src/app/core/interfaces/signup-form.interface.ts
+++
@@ -1,8 +0,0 @@
-import { FormControl } from '@angular/forms';
-
-export interface SignupForm {
-  name: FormControl;
-  email: FormControl;
-  password: FormControl;
-  passwordConfirm: FormControl;
-}
